96ef50bc2893ba70fb6c1b2bbaa8e3f3e3f7e1cf
Admin <Admin@DESKTOP-5QDD06O>
2020-03-10 22:04:21 +0700

    day thong tin request vao db

--- OHD_Project_Sem_3/Controllers/HomeController.cs
+++ OHD_Project_Sem_3/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OHD_Project_Sem_3.Models;
 
 namespace OHD_Project_Sem_3.Controllers
 {
     public class HomeController : Controller
     {
+        private MyDbContext dbContext = new MyDbContext();
         // GET: Home
         public ActionResult Index()
         {
@@ -17,6 +19,23 @@ namespace OHD_Project_Sem_3.Controllers
         public ActionResult ListRequest()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Store( string message, string category, string facility )
+        {
+            var request = new Request
+            {
+                
+                Message = message,
+                Category = category,
+                Facility = facility,
+                CreatedAt = DateTime.Now,
+                Status = RequestStatus.Processing
+            };
+            dbContext.Requests.Add(request);
+            dbContext.SaveChanges();
+            return Redirect("/Home/ListRequest");
         }
     }
 }
