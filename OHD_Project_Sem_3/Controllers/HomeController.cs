5f69816032825bcc1ad50a84dab90d5ae87ff964
Admin <Admin@DESKTOP-5QDD06O>
2020-03-11 20:40:59 +0700

    layout mau

--- OHD_Project_Sem_3/Controllers/HomeController.cs
+++ OHD_Project_Sem_3/Controllers/HomeController.cs
@@ -3,39 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using OHD_Project_Sem_3.Models;
 
 namespace OHD_Project_Sem_3.Controllers
 {
     public class HomeController : Controller
     {
-        private MyDbContext dbContext = new MyDbContext();
         // GET: Home
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult ListRequest()
-        {
-            return View(dbContext.Requests.ToList());
-        }
-
-        [HttpPost]
-        public ActionResult Store( string message, string category, string facility )
-        {
-            var request = new Request
-            {
-                
-                Message = message,
-                Category = category,
-                Facility = facility,
-                CreatedAt = DateTime.Now,
-                Status = RequestStatus.Processing
-            };
-            dbContext.Requests.Add(request);
-            dbContext.SaveChanges();
-            return Redirect("/Home/ListRequest");
         }
     }
 }
