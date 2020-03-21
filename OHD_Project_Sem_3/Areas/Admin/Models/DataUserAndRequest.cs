0eb64563bf00d05377f51a75912378cf406307cd
phongltth1807075 <43373152+phongltth1807075@users.noreply.github.com>
2020-03-21 17:42:45 +0700

    requests

---
+++ OHD_Project_Sem_3/Areas/Admin/Models/DataUserAndRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OHD_Project_Sem_3.Areas.Admin.Models
+{
+    public class DataUserAndRequest
+    {
+        public List<Account> Accounts;
+        public Requests Request;
+    }
+}
--- OHD_Project_Sem_3/Areas/Admin/Models/Requests.cs
+++ OHD_Project_Sem_3/Areas/Admin/Models/Requests.cs
@@ -14,24 +14,27 @@ namespace OHD_Project_Sem_3.Areas.Admin.Models
         public int RequestId { get; set; }
         public string RequestorId { get; set; }
         public virtual Account Requestor { get; set; }
+        public virtual Account Assignor { get; set; }
         public string AssgineeId { get; set; }
         public virtual FacilityCategory FacilityCategory { get; set; }
         public string FacilityCategory_Id { get; set; }
-        public virtual Facility Facility{ get; set; }
+        public virtual Facility Facility { get; set; }
         public string FacilityId { get; set; }
+        [Required(ErrorMessage = "Please enter message.")]
         [Display(Name = "Message")]
+        [StringLength(500, MinimumLength = 2, ErrorMessage = "Message is only allowed between 2 - 500 characters.")]
         public string Remarks { get; set; }
         [Display(Name = "Created At")]
         public DateTime Created_At { get; set; }
         [Display(Name = "Updated At")]
         public DateTime? Updated_At { get; set; }
-        public enum RequestStatus { 
-            Rejected=-1,
-            Closed=0,
-            Unassigned=1,
-            Assigned=2,
-            Word_in_progress=3,
-            Need_more_info=4
+        public enum RequestStatus
+        {
+            Deleted = -1,
+            Done = 0,
+            Processing = 1,
+            Assigned = 2,
+            Waiting = 3,
         }
         [Display(Name = "Status")]
         public RequestStatus Status { get; set; }
